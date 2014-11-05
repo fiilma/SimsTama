7a689d66de88f29749eced876e7ba894ff632015
fiilma <roumboussel@laposte.net>
2014-11-05 12:52:13 +0100

    Changement classe Etre vivant + classe lieux et jeu

---
+++ SimsTama/src/simstama/Lieux.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simstama;
+
+/**
+ *
+ * @author roumi
+ */
+public class Lieux {
+   
+    private String nom;
+    
+    
+    //Methodes
+    
+    
+    public static void menuJeu(){
+        
+    /*
+            * Switch
+     * 1/ Quitter 
+     * 2/ Aller vers => Afficher les autres lieux 
+     * 3/ Sauvegarder 
+       4/ Charger une autre partie 
+     * 5/ Les jauges 
+        6/ Voir objets
+     * 
+     */
+    
+    }
+
+    
+    //************GET etSET *************//
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @param nom the nom to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+    
+    
+}
---
+++ SimsTama/src/simstama/Maison.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simstama;
+
+/**
+ *
+ * @author roumi
+ */
+public class Maison extends Lieux{
+    
+    
+    
+    
+    //Constructeurs 
+    
+    public Maison(){
+    
+    this.setNom("maison");
+    }
+    
+    
+    //Methodes
+    
+    /*
+    public void menuMaison(Tamagoshi monTama){
+    
+    /*
+        Switch 
+        -manger 
+        -boire-
+        -se laver 
+        -telephoner
+        -se faire beau
+        -jouer 
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    }    
+        */
+ 
+}
